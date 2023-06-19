bbd521d396a5764d1da0990db18cacb78a60a532
kirill <kirillaromad@mail.ru>
2023-06-19 23:47:56 +0300

    удален неиспользуемый код

--- sds_server/pdf_utils.py
+++ sds_server/pdf_utils.py
@@ -17,15 +17,6 @@ def download_pdf_from_url(url):
         raise Exception(f"Не удалось скачать PDF по URL: {url}")
 
 
-async def download_pdf_from_url_async(url):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            return response.content
-        else:
-            raise Exception(f"Не удалось скачать PDF по URL: {url}")
-
-
 def convert_pdf_to_text(pdf_content):
     doc = fitz.open("pdf", pdf_content)
     text = ""
@@ -35,23 +26,8 @@ def convert_pdf_to_text(pdf_content):
     return text
 
 
-async def convert_pdf_to_text_async(pdf_content):
-    loop = asyncio.get_running_loop()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        text = await loop.run_in_executor(executor, convert_pdf_to_text, pdf_content)
-
-    return text
-
-
 def process_url(url):
     pdf_content = download_pdf_from_url(url)
     pdf_text = convert_pdf_to_text(pdf_content)
-
-    return pdf_text
-
-
-async def process_url_async(url):
-    pdf_content = await download_pdf_from_url_async(url)
-    pdf_text = await convert_pdf_to_text_async(pdf_content)
 
     return pdf_text
